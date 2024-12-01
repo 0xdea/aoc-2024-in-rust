1953d6ff641a98cf96a71b962038496e9e89484d
raptor <raptor@0xdeadbeef.info>
2024-12-01 06:55:46 +0100

    Second part solved

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -2,6 +2,8 @@ use adv_code_2024::*;
 use anyhow::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
+use itertools::Itertools;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -63,15 +65,45 @@ fn main() -> Result<()> {
     println!("\n=== Part 2 ===");
 
     fn part2<R: BufRead>(reader: R) -> Result<usize> {
-        Ok(0)
+        let mut v1 = vec![];
+        let mut v2 = vec![];
+
+        for line in reader.lines() {
+            let line = line?;
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            if parts.len() != 2 {
+                panic!();
+            }
+            let n1: i32 = parts[0].parse()?;
+            let n2: i32 = parts[1].parse()?;
+            v1.push(n1);
+            v2.push(n2);
+        }
+
+        let counts = count_occurrences(&v1, &v2);
+
+        let answer = v1.iter().fold(0, |acc, &x| {
+            acc + x as usize * counts.get(&x).unwrap_or(&0usize)
+        });
+
+        Ok(answer)
     }
 
-    assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+    assert_eq!(31, part2(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part2(input_file)?);
     println!("Result = {}", result);
-    //endregion
-
     Ok(())
+    // Result = 24931009
+    //endregion
+}
+
+fn count_occurrences(v1: &Vec<i32>, v2: &Vec<i32>) -> HashMap<i32, usize> {
+    let mut counts = HashMap::new();
+    for &n in v1 {
+        let count = v2.iter().filter(|&&x| x == n).count();
+        counts.insert(n, count);
+    }
+    counts
 }
