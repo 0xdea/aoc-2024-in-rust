47943800e75590db9ece998e628bd4d4828d17e2
raptor <raptor@0xdeadbeef.info>
2024-12-01 06:27:24 +0100

    Test input works

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -9,8 +9,13 @@ const DAY: &str = "01";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 const TEST: &str = "\
-<TEST-INPUT>
-"; // TODO: Add the test input
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -18,14 +23,35 @@ fn main() -> Result<()> {
     //region Part 1
     println!("=== Part 1 ===");
 
-    fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        // TODO: Solve Part 1 of the puzzle
-        let answer = reader.lines().flatten().count();
+    fn part1<R: BufRead>(reader: R) -> Result<i32> {
+        let mut v1 = vec![];
+        let mut v2 = vec![];
+
+        for line in reader.lines() {
+            let line = line?;
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            if parts.len() != 2 {
+                panic!();
+            }
+            let n1: i32 = parts[0].parse()?;
+            let n2: i32 = parts[1].parse()?;
+            v1.push(n1);
+            v2.push(n2);
+        }
+
+        v1.sort();
+        v2.sort();
+
+        let answer = v1
+            .iter()
+            .zip(v2.iter())
+            .map(|(n1, n2)| (n1 - n2).abs())
+            .sum();
+
         Ok(answer)
     }
 
-    // TODO: Set the expected answer for the test input
-    assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
+    assert_eq!(11, part1(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part1(input_file)?);
