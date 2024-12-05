1b69c786c7bb5dee154e5c9c0c8c90604247fc06
raptor <raptor@0xdeadbeef.info>
2024-12-05 18:01:53 +0100

    Solved part 1 (better late than never;)

---
+++ src/bin/04.rs
@@ -0,0 +1,130 @@
+use adv_code_2024::*;
+use anyhow::*;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const DAY: &str = "04";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+";
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        let mut answer = 0;
+        let is_xmas = |s| s == "XMAS" || s == "SAMX";
+
+        let input: Vec<Vec<char>> = reader
+            .lines()
+            .map(|l| l.unwrap().chars().collect())
+            .collect();
+        let xlen = input[0].len();
+        let ylen = input.len();
+
+        // Horizontal
+        #[allow(clippy::needless_range_loop)]
+        for x in 0..xlen - 3 {
+            for y in 0..ylen {
+                if is_xmas(format!(
+                    "{}{}{}{}",
+                    input[y][x],
+                    input[y][x + 1],
+                    input[y][x + 2],
+                    input[y][x + 3]
+                )) {
+                    answer += 1;
+                }
+            }
+        }
+
+        // Vertical
+        for x in 0..xlen {
+            for y in 0..ylen - 3 {
+                if is_xmas(format!(
+                    "{}{}{}{}",
+                    input[y][x],
+                    input[y + 1][x],
+                    input[y + 2][x],
+                    input[y + 3][x]
+                )) {
+                    answer += 1;
+                }
+            }
+        }
+
+        // First diagonal
+        for x in 0..xlen - 3 {
+            for y in 0..ylen - 3 {
+                if is_xmas(format!(
+                    "{}{}{}{}",
+                    input[y][x],
+                    input[y + 1][x + 1],
+                    input[y + 2][x + 2],
+                    input[y + 3][x + 3]
+                )) {
+                    answer += 1;
+                }
+            }
+        }
+
+        // Second diagonal
+        for x in 3..xlen {
+            for y in 0..ylen - 3 {
+                if is_xmas(format!(
+                    "{}{}{}{}",
+                    input[y][x],
+                    input[y + 1][x - 1],
+                    input[y + 2][x - 2],
+                    input[y + 3][x - 3]
+                )) {
+                    answer += 1;
+                }
+            }
+        }
+
+        Ok(answer)
+    }
+
+    assert_eq!(18, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {result}");
+    // Result = 2462
+    //endregion
+
+    //region Part 2
+    /*
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        Ok(0)
+    }
+
+    assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {result}");
+    */
+    //endregion
+
+    Ok(())
+}
