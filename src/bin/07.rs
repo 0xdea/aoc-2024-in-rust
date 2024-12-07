dbf84bb203bb88777fa7280ccf9627eb2186949d
raptor <raptor@0xdeadbeef.info>
2024-12-07 07:18:21 +0100

    Working on it... almost there

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -2,6 +2,7 @@ use anyhow::*;
 use aoc_2024_in_rust::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
+use itertools::Itertools;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -9,8 +10,16 @@ const DAY: &str = "07";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 const TEST: &str = "\
-<TEST-INPUT>
-"; // TODO: Add the test input
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+";
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -19,10 +28,44 @@ fn main() -> Result<()> {
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        let mut answer = reader.lines().count();
-        answer += 1;
+        let mut answer = 0;
 
-        // TODO: Solve Part 1 of the puzzle
+        let operators = &['+', '*'];
+
+        for line in reader.lines() {
+            let line = line?;
+
+            let v: Vec<usize> = line
+                .replace(":", "")
+                .split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect();
+
+            let operators_num = v.len() - 2;
+            let perms = permutations(operators, operators_num);
+
+            for perm in perms {
+                let mut res = *&v[1..].first().unwrap().to_owned();
+
+                for (i, operand) in v[1..].iter().enumerate().clone() {
+                    if res > *v.first().unwrap() {
+                        break;
+                    }
+
+                    match perm.chars().nth(i) {
+                        Some('+') => res += operand,
+                        Some('*') => res *= operand,
+                        c => panic!("{c:?}"),
+                    }
+                }
+
+                if res == *v.first().unwrap() {
+                    answer += res;
+                    break;
+                }
+            }
+        }
+
         Ok(answer)
     }
 
@@ -53,4 +96,12 @@ fn main() -> Result<()> {
     //endregion
 
     Ok(())
+}
+
+fn permutations(symbols: &[char], length: usize) -> Vec<String> {
+    std::iter::repeat(symbols.iter())
+        .take(length)
+        .multi_cartesian_product()
+        .map(|v| v.into_iter().collect())
+        .collect()
 }
