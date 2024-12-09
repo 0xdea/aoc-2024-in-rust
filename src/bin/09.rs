671dcc98645f9e28a72eda1b5ffa882e8c7bdb16
raptor <raptor@0xdeadbeef.info>
2024-12-09 15:25:51 +0100

    Solved part 1

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -2,7 +2,6 @@ use anyhow::*;
 use aoc_2024_in_rust::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
-use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -13,25 +12,59 @@ const TEST: &str = "\
 2333133121414131402
 ";
 
-#[derive(Debug)]
-struct Blocks(Vec<Option<i32>>);
+type Block = Option<i32>;
 
+#[derive(Debug)]
+struct Blocks(Vec<Block>);
+
+#[allow(
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_sign_loss
+)]
 impl Blocks {
     fn new<R: BufRead>(mut reader: R) -> Self {
         let mut input = String::new();
-        reader.read_line(&mut input);
+        reader
+            .read_line(&mut input)
+            .expect("error processing input");
 
         let mut blocks = vec![];
-
         for (i, c) in input.trim().chars().enumerate() {
             let size = c.to_digit(10).unwrap() as i32;
-
             let block = if i % 2 == 0 { Some(i as i32 / 2) } else { None };
-
             blocks.extend((0..size).map(|_| block));
         }
 
         Self(blocks)
+    }
+
+    fn get(&self, index: usize) -> Option<Block> {
+        self.0.get(index).copied()
+    }
+
+    fn get_mut(&mut self, index: usize) -> Option<&mut Block> {
+        self.0.get_mut(index)
+    }
+
+    fn remove(&mut self, index: usize) -> Block {
+        self.0.remove(index)
+    }
+
+    fn iter(&self) -> impl DoubleEndedIterator<Item = Block> + ExactSizeIterator + '_ {
+        self.0.iter().copied()
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn checksum(&self) -> usize {
+        self.iter()
+            .map_while(|block| block)
+            .enumerate()
+            .map(|(i, id)| i * id as usize)
+            .sum()
     }
 }
 
@@ -42,13 +75,28 @@ fn main() -> Result<()> {
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(mut reader: R) -> Result<usize> {
-        //let disk = Disk::new(reader);
-        //dbg!(&disk);
+        // Process input disk
+        let mut blocks = Blocks::new(&mut reader);
+        // dbg!(&blocks);
 
-        let blocks = Blocks::new(&mut reader);
-        dbg!(&blocks);
+        // Defrag input disk
+        let mut i = 0;
+        let mut j = blocks.len() - 1;
+        while i < j {
+            if blocks.get(i).unwrap().is_some() {
+                i += 1;
+            } else {
+                *blocks.get_mut(i).unwrap() = blocks.remove(j);
+                j = blocks
+                    .iter()
+                    .enumerate()
+                    .rev()
+                    .find_map(|(idx, block)| block.map(|_| idx))
+                    .unwrap_or_default();
+            }
+        }
 
-        Ok(0)
+        Ok(blocks.checksum())
     }
 
     assert_eq!(1928, part1(BufReader::new(TEST.as_bytes()))?);
@@ -56,6 +104,7 @@ fn main() -> Result<()> {
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part1(input_file)?);
     println!("Result = {result}");
+    // Result = 6398252054886
     //endregion
 
     //region Part 2
