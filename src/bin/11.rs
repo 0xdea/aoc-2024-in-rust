c9e6aa25cc5ca04dc2bdd85ae09bd22685de9f8e
raptor <raptor@0xdeadbeef.info>
2024-12-11 07:25:02 +0100

    Solved part 2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -21,7 +21,6 @@ fn main() -> Result<()> {
 
     fn part1<R: BufRead>(mut reader: R) -> Result<usize> {
         let mut stones = parse_input(&mut reader);
-        dbg!(&stones);
 
         for _ in 0..25 {
             stones = blink(&stones);
@@ -35,24 +34,28 @@ fn main() -> Result<()> {
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part1(input_file)?);
     println!("Result = {result}");
+    // Result = 182081
     //endregion
 
     //region Part 2
-    /*
     println!("\n=== Part 2 ===");
 
-    fn part2<R: BufRead>(reader: R) -> Result<usize> {
-        let mut answer = 0;
+    fn part2<R: BufRead>(mut reader: R) -> Result<usize> {
+        let mut stones = parse_input(&mut reader);
 
-        Ok(answer)
+        for _ in 0..75 {
+            stones = blink(&stones);
+        }
+
+        Ok(stones.values().sum())
     }
 
-    assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+    //assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part2(input_file)?);
     println!("Result = {result}");
-    */
+    // Result = 216318908621637
     //endregion
 
     Ok(())
@@ -71,20 +74,29 @@ fn parse_input<R: BufRead>(mut reader: R) -> HashMap<isize, usize> {
 }
 
 fn blink(stones: &HashMap<isize, usize>) -> HashMap<isize, usize> {
+    // Key = stone mark
+    // Value = stone count
     let mut result = HashMap::new();
-    for (stone, count) in stones {
-        match *stone {
-            0 => *result.entry(1).or_default() += count,
-            _ => {
-                let digits = format!("{}", *stone).len() as u32;
-                if digits % 2 == 0 {
-                    *result.entry(stone % 10_isize.pow(digits / 2)).or_default() += count;
-                    *result.entry(stone / 10_isize.pow(digits / 2)).or_default() += count;
-                } else {
-                    *result.entry(stone * 2024).or_default() += count;
-                }
+
+    for (mark, count) in stones {
+        // Replace mark 0 with mark 1
+        if *mark == 0 {
+            *result.entry(1).or_default() += count;
+        } else {
+            #[allow(clippy::cast_possible_truncation)]
+            let digits = format!("{}", *mark).len() as u32;
+
+            // Replace the stone with two stones
+            if digits % 2 == 0 {
+                *result.entry(mark % 10_isize.pow(digits / 2)).or_default() += count;
+                *result.entry(mark / 10_isize.pow(digits / 2)).or_default() += count;
+
+            // Multiply mark by 2024
+            } else {
+                *result.entry(mark * 2024).or_default() += count;
             }
         }
     }
+
     result
 }
